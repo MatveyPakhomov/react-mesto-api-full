d20d4037b7f014efb8078da8bdc69dbe520caaed
Matvey Pakhomov <matwick1@yandex.ru>
2022-01-28 18:55:30 +0400

    update controllers/users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -32,7 +32,7 @@ function login(req, res, next) {
         .status(200)
         .cookie("jwt", token, {
           maxAge: 3600000 * 24 * 7,
-          // secure: true,
+          secure: false,
           domain: "pakhomov.students.nomoredomains.rocks",
         })
         .send({ message: "Аутентификация пройдена" })
