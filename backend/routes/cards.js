0f5526346679988cd4e4c7d871a6cb532b6f0828
Matvey Pakhomov <matwick1@yandex.ru>
2022-01-30 04:05:43 +0400

    fix

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -13,7 +13,7 @@ const {
 router.post(
   "/cards",
   celebrate({
-    params: Joi.object().keys({
+    body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
       link: Joi.string().required().custom(isValidURL),
     }),
